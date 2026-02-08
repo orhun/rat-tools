df950fa5fe7b9a25866754b719b27394d81358e5
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2026-02-09 02:14:07 +0300

    Revert "Add simple morse decoder"
    This reverts commit a671664e7d0d16b438062c9631400deec968eb1b.

--- antui/src/main.rs
+++ antui/src/main.rs
@@ -7,7 +7,6 @@ use alloc::boxed::Box;
 use cortex_m::prelude::_embedded_hal_timer_CountDown;
 // Linked-List First Fit Heap allocator (feature = "llff")
 use embedded_alloc::LlffHeap as Heap;
-use embedded_hal::digital::{InputPin, OutputPin};
 use mousefood::{EmbeddedBackend, EmbeddedBackendConfig};
 use ratatui::Terminal;
 use rp2040_panic_usb_boot as _;
@@ -32,8 +31,6 @@ use usb_device::{
     prelude::{UsbDeviceBuilder, UsbDeviceState, UsbVidPid},
 };
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
-
-mod app;
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
@@ -110,9 +107,6 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut button = pins.gpio2.into_pull_up_input();
-    let mut buzzer = pins.gpio6.into_push_pull_output();
-
     usb_log(&mut serial, "before i2c");
     let sda = pins
         .gpio0
@@ -154,25 +148,25 @@ fn main() -> ! {
     };
 
     let backend = EmbeddedBackend::new(&mut display, config);
-    let mut terminal = Terminal::new(backend).unwrap();
-    let mut app = app::App::new();
+    let mut terminal = match Terminal::new(backend) {
+        Ok(t) => t,
+        Err(_) => {
+            usb_log(&mut serial, "terminal new error");
+            loop {
+                delay.start(50.millis());
+                let _ = nb::block!(delay.wait());
+            }
+        }
+    };
 
     loop {
-        let now_ms = timer.get_counter().ticks() / 1000;
-        let button_pressed = button.is_low().unwrap_or(false);
-        app.tick(now_ms, button_pressed);
-
-        if app.buzzer_on() {
-            let _ = buzzer.set_high();
-        } else {
-            let _ = buzzer.set_low();
+        if let Err(_) = terminal.draw(|f| {
+            let area = f.area();
+            let block = ratatui::widgets::Block::bordered().title("rat");
+            f.render_widget(block, area);
+        }) {
+            usb_log(&mut serial, "terminal draw error");
         }
-
-        terminal
-            .draw(|f| {
-                app.render(f);
-            })
-            .unwrap();
 
         usb_log(&mut serial, "loop");
 
